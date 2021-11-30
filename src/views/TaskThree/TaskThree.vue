05f8df52bae03213c19da6e1e0491372e55de388
cdheij <Chris.Heij@logius.nl>
2021-11-30 16:49:11 +0100

    add task three

---
+++ src/views/TaskThree/TaskThree.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="prose">
+    <h1>Opdracht 3: "Skip link"</h1>
+
+    <div class="userstory">
+      <strong>Als</strong> gebruiker met een visuele beperking<br />
+      <strong>wil ik</strong> een skiplink tegenkomen als eerste focusbare
+      element<br />
+      <strong>zodat</strong> efficient kan navigeren naar de hoofdinhoud
+    </div>
+
+    <h2>Acceptatiecriteria</h2>
+    <ul>
+      <li>Het element is niet zichtbaar als het geen focus heeft</li>
+      <li>Als er op geklikt krijgt de main content zichtbare focus</li>
+    </ul>
+
+    <h2>Bonuspunten</h2>
+    <ul>
+      <li>...</li>
+    </ul>
+
+    <router-link to="/opdracht-3-solution" class="button mt-8 mr-2">
+      <strong>Opdracht 3 Uitwerking</strong>
+    </router-link>
+    <router-link to="/opdracht-3" class="button mt-8">
+      Start&nbsp;<strong>Opdracht 3</strong>
+    </router-link>
+  </div>
+</template>
